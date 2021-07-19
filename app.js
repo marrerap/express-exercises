95e97b769ed4d8fb8fd025e6fcf37ea081cd2e6a
marrerap <andrew.marrero@outlook.com>
2021-07-19 16:00:36 -0500

    finished the express exercies

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const express = require('express');
-const db = require('./db')
+const db = require('./model/db.js')
 
 
 
@@ -18,50 +18,29 @@ const server = http.createServer(app)
 // root route
 app.get('/', (req, res) => {
   res.render('home', {
-    title: "JJ's Diner",
-    special: "Free Bacon",
-    user: null
+    title: "CEO Reference!!!",    
   });
 })
-app.get('/menu', (req, res) => {
-  res.render('menu', {
-    title: "JJ's Menu",
+app.get('/ceo-list', (req, res) => {
+  res.render('ceo-list', {
+    title: "Ceo's",
     // db.specials is allowing me to pull an list from an object in the db.js
-    specials: db.specials
-  })
-})
-app.get("/menu/:id", (req,res) => {
-    // get data fomr the db.specials
-    const foundSpecial = db.specials.find((special) => {
-      return special.id === parseInt(req.params.id)
-    })  
-  // render the template
-  res.render("menuItem", {
-    title: "JJ's Menu",
-    special: foundSpecial
-  })
-})
-
-app.get('/hello', (req, res) => {
-  res.send('Hello World')
-})
-app.get("/api/friends", (req, res) => {
-  res.json(db.friends)
-})
-
-
-app.get('/api/friends/:handle', (req, res) => {
-  console.log(req.params.handle)
-  const foundFriends =db.friends.find((friend) => {
-    if (friend.handle === req.params.handle) {
-      return true
-    } else {
-      return false
-    }
     
   })
-  res.json(foundFriends)
 })
+
+app.get("/ceo-details/:slug", (req,res) => {
+    // get data from the db.specials
+    const foundCeo = db.find((ceo) => {
+      return ceo.id === parseInt(req.params.slug)
+    })  
+  // render the template
+  res.render("ceo-details", {
+    title: "Ceo Details",
+    Ceo: foundCeo
+  })
+})
+
 
 app.get('/*', (req, res) => {
   res.status(404)
